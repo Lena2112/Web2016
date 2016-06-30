f38fdd14f131cac01decb86a4e28dc15d8a09904
Lena2112 <Helen.MONSTRIK@gmail.com>
2016-06-30 23:55:16 +0300

    были найдены и удалены табы

--- project_final/js/click_processing.js
+++ project_final/js/click_processing.js
@@ -1,13 +1,13 @@
 $(document).on('click', ".arrow_next",function(){ 
-	var carousel = $(this).parents('.selecting_unit');
-	bottom_carousel(carousel);
-	return false;
+    var carousel = $(this).parents('.selecting_unit');
+    bottom_carousel(carousel);
+    return false;
 });
 
 $(document).on('click',".arrow_prev",function(){ 
-	var carousel = $(this).parents('.selecting_unit');
-	top_carousel(carousel);
-	return false;
+    var carousel = $(this).parents('.selecting_unit');
+    top_carousel(carousel);
+    return false;
 });
 
 function top_carousel(carousel)
@@ -23,7 +23,7 @@ function bottom_carousel(carousel)
 {
    var block_height = $(carousel).find('li').outerHeight();
    $(carousel).find("ul").animate({top: "-"+ block_height +"px"}, 500, function(){
-	    $(carousel).find("ul li").eq(0).clone().appendTo($(carousel).find("ul")); 
+        $(carousel).find("ul li").eq(0).clone().appendTo($(carousel).find("ul")); 
         $(carousel).find("ul li").eq(0).remove(); 
         $(carousel).find("ul").css({"top":"0px"});
    }); 
@@ -32,11 +32,11 @@ function bottom_carousel(carousel)
 $('.go_to_message').on('click', function(){ 
     $('#right_block').css("display", "none"); 
     $('#right_block_2').css("display", "block");
-	return false;
+    return false;
 });
 
 $('.go_to_choose').on('click', function(){ 
     $('#right_block').css("display", "block"); 
     $('#right_block_2').css("display", "none");
-	return false;
+    return false;
 });
--- project_final/taken_from_the_database.php
+++ project_final/taken_from_the_database.php
@@ -1,21 +1,21 @@
 ﻿<?php 
     require_once("include/database.inc.php");
     
-	$background ='';
+    $background ='';
     $image = '';
     $message = '';
-	
+    
     if (isset($_GET['background'])&&isset($_GET['image'])&&isset($_GET['message']))
     {
         $background = $_GET['background'];
-		$image = $_GET['image'];
-		$message = $_GET['message'];
+        $image = $_GET['image'];
+        $message = $_GET['message'];
     }
     if (!empty($_GET['background'])&&!empty($_GET['image'])&&!empty($_GET['message']))
     {
         dbInitialConnect();
-		AddingData($background, $image, $message);
-		mysql_close();
+        AddingData($background, $image, $message);
+        mysql_close();
     }
     else 
     {
@@ -25,9 +25,9 @@
   
  
   
-	
+    
   
 
-	
+    
 
   
