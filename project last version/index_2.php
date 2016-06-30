2d8dc2c1812966c0f15608c2cfa9b08b336ecf37
Lena2112 <Helen.MONSTRIK@gmail.com>
2016-06-30 18:15:27 +0300

    Посылает запрос BD. Берет массив по id.

---
+++ project last version/index_2.php
@@ -0,0 +1,9 @@
+﻿<?php
+    require_once("include/mysql_work.inc.php");
+    $connect = mysql_connect('localhost:3306', 'root');
+    mysql_select_db('mothers_day', $connect);
+    
+    $row = GetAllData($_GET['id']);
+    echo json_encode($row);
+    mysql_close();
+  
