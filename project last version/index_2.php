6db6e23230b6c8691ac53402290bb28dc248b211
Lena2112 <Helen.MONSTRIK@gmail.com>
2016-07-01 00:14:35 +0300

    delete

--- project last version/index_2.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-    require_once("include/mysql_work.inc.php");
-    $connect = mysql_connect('localhost:3306', 'root');
-    mysql_select_db('mothers_day', $connect);
-    
-    $row = GetAllData($_GET['id']);
-    echo json_encode($row);
-    mysql_close();
-  
